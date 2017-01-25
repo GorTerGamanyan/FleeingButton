36cb89d20638ab29dda254d6a4cc71106bf2bfe1
Gor777777777 <gorgoar@gmail.com>
2017-01-25 12:39:43 +0400

    Created escaping button

---
+++ FleeingButton/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FleeingButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+
+            Random rnd = new Random();
+
+            button1.Location = new Point(rnd.Next(0, 1100), rnd.Next(0, 600));
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
